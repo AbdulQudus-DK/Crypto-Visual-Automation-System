de24fbf2c2eb68768946afd1397428f4e8c0ed68
AbdulQudus-DK <abdulsalamabdulqudus79@gmail.com>
2025-11-01 10:42:15 +0100

    Add safe config file using environment variables

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
-MONGODB_URI = "mongodb+srv://splexdb:Sp1exx-db@splex-godb.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
+import os
+from dotenv import load_dotenv
 
-MONGO_DB_NAME = "rss_news_db"
-MONGO_COLLECTION_NAME = "rss_articles"
-OPENAI_API_KEY = "your_openai_api_key"
-TELEGRAM_BOT_TOKEN = "8307131116:AAHGda4orY9XTxyowo19X-5eHO5S3ztJci0"
-TELEGRAM_CHANNEL_ID = "-1003196497411"
-# === Scheduler Settings ===
-SCHEDULE_INTERVAL_HOURS = 2
-# === Media Settings ===
-MEDIA_FOLDER = "media"
-BACKGROUND_MUSIC = f"{MEDIA_FOLDER}/bg_music.mp3"
-VIDEO_DURATION = 10
+# Load environment variables from .env file (locally)
+load_dotenv()
+
+# Database
+MONGODB_URI = os.getenv("MONGODB_URI")
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "CryptoDB")
+MONGO_COLLECTION_NAME = os.getenv("MONGO_COLLECTION_NAME", "articles")
+
+# Media and video
+MEDIA_FOLDER = os.getenv("MEDIA_FOLDER", "media")
+BACKGROUND_MUSIC = os.getenv("BACKGROUND_MUSIC", "assets/background.mp3")
+VIDEO_DURATION = int(os.getenv("VIDEO_DURATION", 15))
